53bb90df5a55fe6a2588d8180f5c4f5e20ad4e5d
VikaSeredenina <119499762+VikaSeredenina@users.noreply.github.com>
2022-12-06 18:28:48 +0300

    Add files via upload

---
+++ Zadania 5 (form)/Zadania 5 (form)/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadania_5__form_
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double f(double x)
+    {
+        try
+        {
+            //если х не попадает в область определения, то генерируется исключение 
+            if ((x * x + 2 * x + 1) == 0 || (x * x + 2 * x + 1) < 0) throw new Exception(); //оператор throw создает исключение 
+            else return ((3 * x + 4) / (Math.Sqrt(x * x + 2 * x + 1)));
+        }
+        catch
+        {
+            throw;
+        }
+    }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+               double a = double.Parse(textBox1.Text);
+              double  b = double.Parse(textBox2.Text);
+                double h = double.Parse(textBox3.Text);
+                if (a > b)
+                {
+                    MessageBox.Show("'a' не может быть больше 'b'");
+                    return;
+                }
+
+             h = double.Parse(textBox3.Text);
+                if (h <= 0)
+                {
+                    MessageBox.Show("Шаг должен быть больше 0!");
+                    return;
+                }
+
+                for (double i = a; i <= b; i += h)
+                    try
+                    {
+                        richTextBox1.Text += $"y({Math.Round(i, 2)})={Math.Round(f(i), 4)}" + Environment.NewLine;
+                    }
+                    catch
+                    {
+                        richTextBox1.Text += $"y({i})=функция не определена" + Environment.NewLine;
+                    }
+             }
+
+            catch (FormatException)
+            {
+
+                MessageBox.Show("Некорректные данные");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Clear();
+            textBox2.Text = "";
+            textBox2.Clear();
+            textBox3.Text = "";
+            textBox3.Clear();
+            richTextBox1.Text = "";
+            richTextBox1.Clear();
+        }
+    }
+}
+
---
+++ Zadania 5/Zadania 5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania_5
+{
+    internal class Program
+    {
+        static double f(double x)
+        {
+             //если х не попадает в область определения, то генерируется исключение 
+                if ((x*x+2*x+1)==0  || (x * x + 2 * x + 1)<0) throw new Exception(); //оператор throw создает исключение 
+                else return ((3*x+4)/(Math.Sqrt(x*x+2*x+1)));
+            
+        }
+
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+         
+            try
+            {
+                Console.Write("a=");
+                 a = double.Parse(Console.ReadLine());
+                Console.Write("b=");
+                 b = double.Parse(Console.ReadLine());
+                        Console.Write("h=");
+                       double  h = double.Parse(Console.ReadLine());
+                if (a > b)
+                {
+                    Console.WriteLine("'a' не может быть больше'b'");
+                }
+                else if (h <= 0)
+                {
+                    Console.WriteLine("Шаг должен быть больше 0");
+                }
+                for (double i = a; i <= b; i += h)
+                    try
+                    {
+                        Console.WriteLine("y({0})={1:f4}", i, f(i));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("y({0})=функция не определена", i);
+                    }
+
+            }
+            //Проверка на ввод 
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректные данные");
+            }
+
+            catch
+            {
+                Console.WriteLine("Неизвестная ошибка");
+            }
+
+
+        }
+
+
+    }
+
+
+}
+
